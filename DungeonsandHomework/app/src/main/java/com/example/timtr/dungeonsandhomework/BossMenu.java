a805e4f0afb3a7024570d0ee4ea97924b4d3ec31
Tim Tran <ttran1@ualberta.ca>
2017-11-18 13:29:50 -0700

    Add BossMenu Activity

---
+++ DungeonsandHomework/app/src/main/java/com/example/timtr/dungeonsandhomework/BossMenu.java
@@ -0,0 +1,13 @@
+package com.example.timtr.dungeonsandhomework;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class BossMenu extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_boss_menu);
+    }
+}
