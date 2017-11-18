996f09b33efeeb2aa8badbb953ab2432898d6d60
Tim Tran <ttran1@ualberta.ca>
2017-11-18 13:46:42 -0700

    Add button from MainMenu to BossMenu

--- DungeonsandHomework/app/src/main/java/com/example/timtr/dungeonsandhomework/MainMenu.java
+++ DungeonsandHomework/app/src/main/java/com/example/timtr/dungeonsandhomework/MainMenu.java
@@ -1,7 +1,11 @@
 package com.example.timtr.dungeonsandhomework;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainMenu extends AppCompatActivity {
 
@@ -9,5 +13,13 @@ public class MainMenu extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
+    }
+
+    /** Called when the user touches the button */
+    public void OpenBossMenu(View view) {
+        Toast.makeText(this, "open boss menu", Toast.LENGTH_LONG).show();
+
+        Intent intent = new Intent(this, BossMenu.class);
+        startActivity(intent);
     }
 }
