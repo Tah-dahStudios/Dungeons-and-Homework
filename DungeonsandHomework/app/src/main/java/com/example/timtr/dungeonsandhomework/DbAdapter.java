141e249305f25aaf020f8c2da13175d2b4ef4de8
AY-Chen <yin6@ualberta.ca>
2017-11-18 14:32:45 -0700

    Added basic DB interaction class

---
+++ DungeonsandHomework/app/src/main/java/com/example/timtr/dungeonsandhomework/DbAdapter.java
@@ -0,0 +1,61 @@
+package com.example.timtr.dungeonsandhomework;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+/**
+ * Created by Alex Chen on 2017-11-18.
+ */
+
+public class DbAdapter {
+    /**
+     * Class for interacting with the database. Allows updating of item counts.
+     */
+    dbHelper myHelper;
+    public DbAdapter(Context context)
+    {
+        myHelper = new dbHelper(context);
+    }
+
+    public useItem()
+
+
+    static class dbHelper extends SQLiteOpenHelper {
+        private static final String DATABASE_NAME = "myDatabase";    // Database Name
+        private static final String ITEM_TABLE = "items";   // Table Name
+        private static final int DATABASE_Version = 1;    // Database Version
+        private static final String ItemID = "IID";     // Column I (Primary Key)
+        private static final String ITEM_NAME = "Name";    //Column II
+        private static final String ITEM_QTY = "Qty";    //Column III
+        private static final String CREATE_TABLE = "CREATE TABLE " + ITEM_TABLE +
+                " (" + ItemID + " INTEGER PRIMARY KEY AUTOINCREMENT, " + ITEM_NAME + " VARCHAR(255) ," + ITEM_QTY + " INTEGER);";
+        private static final String DROP_ITEM_TABLE = "DROP TABLE IF EXISTS " + ITEM_TABLE;
+        private Context context;
+
+        public dbHelper(Context context) {
+            super(context, DATABASE_NAME, null, DATABASE_Version);
+            this.context = context;
+        }
+
+        public void onCreate(SQLiteDatabase db) {
+
+            try {
+                db.execSQL(CREATE_TABLE);
+            } catch (Exception e) {
+                ;
+            }
+        }
+
+        @Override
+        public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+            try {
+//            Message.message(context,"OnUpgrade");
+                db.execSQL(DROP_ITEM_TABLE);
+                onCreate(db);
+            } catch (Exception e) {
+//            Message.message(context,""+e);
+            }
+        }
+    }
+}
